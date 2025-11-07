ab67deee023f9e9912ee5c7abdbb94e9b5e02f24
J.P. Turcotte <26857126+jpturcotte@users.noreply.github.com>
2025-11-07 14:43:56 -0500

    Enhance map widget interactivity

--- code/gui/map_widget.h
+++ code/gui/map_widget.h
@@ -1,13 +1,26 @@
 #ifndef MAP_WIDGET_H
 #define MAP_WIDGET_H
 
-#include <vector>
+#include <functional>
+#include <optional>
 #include <string>
+#include <vector>
+
 #include "imgui.h"
 
 struct Tile {
     char character;
     ImVec4 color;
+};
+
+enum class TileAction {
+    Hover,
+    Click
+};
+
+struct TileSelection {
+    int x = 0;
+    int y = 0;
 };
 
 class MapWidget {
@@ -16,8 +29,18 @@ public:
     ~MapWidget();
 
     void Draw();
+
+    using TileCallback = std::function<void(int x, int y, TileAction action)>;
+    void SetTileCallback(TileCallback callback);
+
+    [[nodiscard]] const ImVec2& GetTileSize() const;
+    [[nodiscard]] std::optional<TileSelection> GetSelectedTile() const;
+
 private:
     std::vector<std::vector<Tile>> mock_map_;
+    ImVec2 tile_size_;
+    TileCallback tile_callback_;
+    std::optional<TileSelection> selected_tile_;
 };
 
 #endif // MAP_WIDGET_H
